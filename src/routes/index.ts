59524129b60378f8e482bb4cf83ff97d09189aa5
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-06 04:42:58 -0300

    Add random-js (npm)

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,14 @@
 import {Router} from 'express'
-
+import { dice, integer,MersenneTwister19937 } from "random-js";
 
 const routes = Router()
 
-routes.get('/',(req,res)=>{res.json('Deu certo')})
+routes.get('/', async(req,res)=>{
+
+     const teste =dice(100,10)(MersenneTwister19937.autoSeed())
+     
+    return res.json(teste)
+
+})
 
 export default routes
