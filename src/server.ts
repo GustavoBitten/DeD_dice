ffae3764e51a5f142a4719667a85eb2a3351288b
GustavoBitten <gustavoduarte1999@hotmail.com>
2020-09-09 21:26:46 -0300

    Add and config socket io

--- src/server.ts
+++ src/server.ts
@@ -3,17 +3,23 @@ import express, { json, Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import routes from './routes'
 import cors from "cors";
+import http from "http";
 
+import SocketIO,{Server} from "socket.io";
+
+const app = express()
+const server = http.createServer(app);
+const io = SocketIO(server);
+
+const PORT : string|number = process.env.PORT || 3344;
+
+app.use(cors())
+app.use(routes)
+app.use(json())
 import './database'
 import AppError from './errors/AppError'
 
-const PORT : string|number = process.env.PORT || 3333;
 
-const app = express()
-
-app.use(cors())
-app.use(json())
-app.use(routes)
 app.use((err: Error, request: Request, response: Response, _: NextFunction) =>{
   if(err instanceof AppError){
     return response.status( err.statusCode).json({
@@ -21,14 +27,31 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) =>{
       message: err.message
     })
   }
-
+  
   console.error(err)
-
+  
   return response.status(500).json({
     status: 'error',
     message: 'Internal server error'
   })
 })
 
+// Test to implements SocketOi
 
-app.listen(PORT,()=>console.log('Server online on port 3333'))
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  
+  socket.on('dice', data => {
+    console.log('[SOCKET] Chat.message => ', data)
+    io.emit('dice',data)
+})
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+  });
+});
+
+
+
+
+server.listen(PORT,()=>console.log('Server online on port 3333'))
